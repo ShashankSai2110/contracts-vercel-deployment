689de46b7cf34bd3f4f3f793cd6443549c22c771
vivishiek <vivishiek.mohan@revealhealthtech.com>
2025-04-29 14:20:52 +0530

    config refactored

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,14 +1,2 @@
 export const API_URL = "http://localhost:8000/api/v1";
 export const WS_URL = `ws://localhost:8000/api/v1/chat/embed_documents`; 
-export const CATEGORIES = {
-  CONTRACT: "contract",
-  SUPPORTING_DOCUMENT: "supportingDocument"
-};
-
-export const ENDPOINTS = {
-  ASK_CUSTOM_QUESTION: "/chat/ask_custom_question",
-  EMBED_DOCUMENTS: "/chat/embed_documents",
-  ASK_PREDEFINED_QUESTION: "/chat/question/{question_id}",
-  GET_QUESTIONS: "/questions",
-  UPLOAD_FILE: "/files"
-};
